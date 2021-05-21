c83c5e5688679a28dde5547cee05e767505b9597
valgring95 <33157@student.pwsztar.edu.pl>
2021-05-21 11:39:27 +0200

    pr7_sortowanieDanych

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./components/home";
+import NavBar from "./components/navbar";
+import { Route, Switch } from "react-router-dom";
+import Posts from './components/posts';
+import LoginForm from "./components/loginForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container-fluid">
+        <NavBar />
+        <div className="container">
+            <div className="content">
+                <Switch>
+                <Route path="/login" component={LoginForm} />
+                    <Route path="/posts"
+                    render={props => <Posts sortBy="newest" {...props} />}
+                    />
+                    <Route path="/" exact component={Home} />
+                </Switch>
+            </div>
+        </div>
+      </div>
   );
-}
-
-export default App;
+ }
+ 
+ export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
+  <BrowserRouter>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <App />
+  </React.StrictMode>
+</BrowserRouter>,
   document.getElementById('root')
 );
 
